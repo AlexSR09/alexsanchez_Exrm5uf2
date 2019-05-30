9d2133049ce8ebc5ba1e5ffa1cdbcff28faa9d3c
AlexSR09 <44049741+AlexSR09@users.noreply.github.com>
2019-05-30 09:06:03 +0200

    Add files via upload

---
+++ Proves/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Proves")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Proves")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8d3a7c31-2ff4-4503-abfe-db37905a1e64")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Proves/Proves.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Llibreria;
+using System.Collections.Generic;
+
+namespace Proves
+{
+    [TestClass]
+    public class Proves
+    {
+        [TestMethod]
+        public void EsPrimer()
+        {
+            bool resultat = true;
+            ClFuncions llb = new ClFuncions();
+            resultat = llb.EsPrimer(3);
+            Assert.AreEqual( true, resultat);
+
+        }
+    }
+}
