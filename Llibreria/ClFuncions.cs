77081980d4f035797537ceaa0f0ca6b4449272aa
AlexSR09 <44049741+AlexSR09@users.noreply.github.com>
2019-05-30 08:58:32 +0200

    Add files via upload

---
+++ Llibreria/ClFuncions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Llibreria
+{
+    public class ClFuncions
+    {
+        public bool EsPrimer(int v1)
+        {
+
+            int i = 2;
+            int resto = 0;
+            while (i < v1)
+            {
+                resto = v1 % i;
+
+                if (resto == 0)
+                {
+
+                    return false;
+
+                }
+
+                i++;
+            }
+            return true;
+
+        }
+
+    }
+   
+}
+
